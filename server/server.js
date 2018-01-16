e4fe569b9440b5d837ded514fd982724e5248ea0
m.nabiya@gmail.com <m.nabiya@gmail.com>
2018-01-15 19:03:22 -0600

    server update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8000 || process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
 	res.send("hello world");
