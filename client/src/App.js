c9d8afcb6e1130209e13ed8a176c74147c316687
Sofia <sz14g@my.fsu.edu>
2018-01-16 11:48:38 -0600

    adding react-router-dom, Navbar compoent, & homepage component content

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,38 @@
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import React, { Component } from 'react';
+import { Navbar } from './components/Navbar/Navbar';
+import { Homepage } from './pages/Homepage';
 import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div classname = "head"> Hello  World</div>
+//     );
+//   }
+// }
+
+// export default App;
+
 
 class App extends Component {
   render() {
     return (
-      <div classname = "head"> Hello  World</div>
+      <Router>
+        <div>
+
+
+          <Navbar />
+           
+          <Route exact path="/" component={Homepage}/>
+
+
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Navbar = () => {
+	return (
+		<ul>
+			<li><Link to="/">Homepage</Link></li> 
+		</ul>
+	)
+}
