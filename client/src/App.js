7356f91811d07073b033ebe3c8378496b3148909
m.nabiya@gmail.com <m.nabiya@gmail.com>
2018-01-24 20:53:51 -0600

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,9 @@ import Kitchen from './pages/Kitchen';
 import Interior from './pages/Interior';
 import Exterior from './pages/Exterior';
 import Navbar from "./components/Navbar/Navbar";
-
 //import connect function from react-redux library. Connect funtion gives certain components the ability to call action creators
 import { connect } from "react-redux";
-
+import Profile from "./pages/Profile";
 //import all our action creators from action file
 import * as actions from "./actions";
 
@@ -17,7 +16,7 @@ class App extends Component {
 // as soon as the component mounts, go ahead and find out if the user is logged in or not
 componentDidMount(){
   this.props.getUser();
-
+  console.log(this.props)
 }
 
   render() {
@@ -30,7 +29,7 @@ componentDidMount(){
           <Route exact path="/kitchen" component={Kitchen}/>
           <Route exact path="/interior" component={Interior}/>
           <Route exact path="/exterior" component={Exterior}/>
-          
+          <Route exact path="/Profile" component={Profile}/>
         </div>
       </Router>
     );
