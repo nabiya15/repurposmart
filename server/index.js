76aa461fc11a96d416378ef43f736a77e0e86ef7
m.nabiya@gmail.com <m.nabiya@gmail.com>
2018-01-27 10:54:43 -0600

    error fix

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ if(process.env.NODE_ENV === "production"){
 	//Express will serve up index.html if route is not recognized
 	const path = require("path");
 	app.get("*", (req, res)=>{
-		res.sendFile(path.resolve(__dirname,"client","build","index,html"));
+		res.sendFile(path.resolve(__dirname,"client","build","index.html"));
 	})	
 }
 
